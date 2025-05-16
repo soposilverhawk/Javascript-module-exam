2c26d2cd782331d252e0d62082b984f5a2aa1107
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-05-17 00:07:48 +0400

    refactored loaheader to be an async function

--- loadHeader.js
+++ loadHeader.js
@@ -1,10 +1,16 @@
-function loadHeader () {
-  fetch("header.html")
-  .then((res) => res.text())
-  .then((data) => {
+async function loadHeader () {
+  try {
+    const res = await fetch ("header.html");
+
+    if(!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data = await res.text();
     const header = document.getElementById("header");
     header.innerHTML = data;
-  })
+  } catch (err) {
+    console.error("Failed to load header:", err)
+  }
 }
 
 export { loadHeader }
