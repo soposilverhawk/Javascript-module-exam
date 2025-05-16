c4297b9a27127d7c0d52ca6986a8f8888cf367a4
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-05-17 00:11:10 +0400

    refactored loadfooter to be an async function

--- loadFooter.js
+++ loadFooter.js
@@ -1,10 +1,16 @@
-function loadFooter () {
-    fetch("footer.html")
-    .then((res) => res.text())
-    .then((data) => {
-        const footer = document.getElementById("footer");
-        footer.innerHTML = data;
-    })
+async function loadFooter() {
+  try {
+    const res = await fetch("footer.html");
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data = await res.text();
+    const footer = document.getElementById("footer");
+    footer.innerHTML = data;
+  } catch (error) {
+    console.error("Failed to load header:", err);
+  }
 }
 
-export { loadFooter }
+export { loadFooter };
